566d4982487a0ca8a5561940bb8ded44ee2e171f
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-11-06 23:29:50 -0200

    questao2-finalizada

--- questao2.asm
+++ questao2.asm
@@ -16,6 +16,7 @@
 	move $t0, $zero
 	move $t3, $zero
 	
+	
 	#i = i +1
 	addi $s1, $s1,1
 	
@@ -26,6 +27,7 @@
 	sw $t0, 0($s0)
 	move $t0, $zero
 	move $t3, $zero
+	la $s0, 0x10010020
 	
 	#i = i+1
 	addi $s1, $s1,1
@@ -37,6 +39,7 @@
 	sw $t0, 0($s0)
 	move $t0, $zero
 	move $t3, $zero
+	la $s0, 0x10010020
 	
 	#i = i+1
 	addi $s1, $s1, 1
@@ -48,6 +51,7 @@
 	sw $t0, 0($s0)
 	move $t0, $zero
 	move $t3, $zero
+	la $s0, 0x10010020
 	
 	#i = i+1
 	addi $s1, $s1, 1
@@ -59,6 +63,7 @@
 	sw $t0, 0($s0)
 	move $t0, $zero
 	move $t3, $zero
+	la $s0, 0x10010020
 	
 	#i = i+1
 	addi $s1, $s1, 1
@@ -70,3 +75,17 @@
 	sw $t0, 0($s0)
 	move $t0, $zero
 	move $t3, $zero
+	la $s0, 0x10010020
+	
+	#i = i+1
+	addi $s1, $s1, 1
+	
+	#v[2] + v[4]
+	lw $t4, 8($s0)
+	lw $t5, 16($s0)
+	add $t0, $t4, $t5
+	#v[2] + v[4] + num
+	add $t0, $t0, $s2
+	mul $t3, $s1,4
+	add $s0, $s0, $t3
+	sw $t0, 0($s0)
